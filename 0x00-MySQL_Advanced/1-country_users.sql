816d45947f8605216a32ab1ac417a27f3fde5c98
badrxd <badrxdxd@gmail.com>
2024-01-17 13:04:00 +0100

    task 1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+-- script that creates a table users following these requirements:
+---- id, integer, never null, auto increment and primary key
+---- email, string (255 characters), never null and unique
+---- name, string (255 characters)
+---- country, enumeration of countries: US, CO and TN, never null
+
+CREATE TABLE IF NOT EXISTS users(
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email varchar(255) NOT NULL UNIQUE,
+        name varchar(255),
+        country ENUM('US','CO','TN') DEFAULT 'US' NOT NULL
+)
