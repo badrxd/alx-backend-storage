bf92d0014949807df695c49c8ca3d1f78330c7f8
badrxd <badrxdxd@gmail.com>
2024-01-23 15:52:20 +0100

    task 8. List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """lists all documents in a collection mongo_collection"""
+    if mongo_collection is not None:
+        return []
+    return [x for x in mongo_collection.find()]
